15c5a49b6f40b87f261c4de188a80ad65ee7495c
Hheokjina <lsh6439ll@gmail.com>
2025-10-22 12:57:46 +0900

    fix(enrollform): updat enrollform update,delete, logic
    - 게시글 내용 확인 후 수정 변경이 로그인이 되어야만 버튼이 활성화되도록 변경

--- 05_Spring/spring/src/main/java/com/kh/spring/config/WebConfig.java
+++ 05_Spring/spring/src/main/java/com/kh/spring/config/WebConfig.java
@@ -5,24 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*
-*
-* */
-
+/**
+ * Srping MVC설정을 위한 Configuration 클래스
+ * WebMvcConfigurer 인터페이스를 구현하여 spring mvc의 다양한 설정을 커스터마이징 할 수 있음.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     /*
-        인터셉터를 등록하는 메서드
-        여러개의 인터페이스를 등록할 수 있고, 등록 순서대로 실행됨.
-        InterceptorRegistry를 통해 인터셉터를 등록
+         인터셉터를 등록하는 메서드
+         여러개의 인터페이스를 등록할 수 있고, 등록 순서대로 실행됨.
+         InterceptorRegistry를 통해 인터셉터를 등록
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
-                .addPathPatterns("/myPage.me",
-                                    "update.me",
-                                    "/delete.me")
-        .excludePathPatterns("/login.me",
+                .addPathPatterns(
+                        "/myPage.me",
+                        "/update.me",
+                        "/delete.me"
+                ).excludePathPatterns("/login.me", "/update.me", "/delete.me");
     }
 }
