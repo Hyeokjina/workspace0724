72a1ae8f9c16fcc157d92cf75eab1fb139bcfa85
Hheokjina <lsh6439ll@gmail.com>
2025-11-12 16:48:20 +0900

    feat(toilet)add toilet controller, service ,jsppage
    - 공중화장실 위치 를 보여주기 위한 컨트롤러 서비스 로직 생성후 페이지구분을 통해 목록을 볼수 있는 jsp페이지생성
    - 서울공공데이터선터에서 화장실 위치를 받고 , 카카오 지도 api를 사용

--- 05_Spring/spring/src/main/java/com/kh/spring/service/ToiletService.java
+++ 05_Spring/spring/src/main/java/com/kh/spring/service/ToiletService.java
@@ -1,24 +1,54 @@
 package com.kh.spring.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.spring.model.dto.ToiletListResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@Service
 public class ToiletService {
 
-        @Value("${seoul.api.key}")
-        private  String apiKey;
+    @Value("${seoul.api.key}")
+    private String apiKey;
 
-        private static final String API_URL = "http://openapi.seoul.go.kr:8088";
+    private static final String API_URL = "http://openapi.seoul.go.kr:8088";
 
-    public void getToileList(int start , int end) {
+    public ToiletListResponse getToiletList(int start,int end){
 
-        String url = String.format("%s/%s/json/mgisToiletPio/%d/%d", API_URL, apikey, start, end);
-        //http://openapi.seoul.go.kr:8088/6c787557716c736838374773514157/json/mgisToiletPoi/1/5/
+        try {
+            //http://openapi.seoul.go.kr:8088/인증키/응답형식/mgisToiletPoi/시작인덱스/끝인덱스/
+            String url = String.format("%s/%s/json/mgisToiletPoi/%d/%d", API_URL, apiKey, start, end);
 
-        log.info("url : {}", url);
+            log.info("url : {}", url);
 
-        RestTemplate restTemplate = new RestTemplate();
-        String jsonResponse = restTemplate.getForObject(url, String.class);
+            RestTemplate restTemplate = new RestTemplate();
+            String jsonResponse = restTemplate.getForObject(url, String.class);
+
+            ToiletListResponse result = parseJsonResponse(jsonResponse);
+
+            return result;
+        } catch (Exception e) {
+            log.error("화장실 목록 조회 실패 ", e);
+            return null;
+        }
+    }
+
+    private ToiletListResponse parseJsonResponse(String jsonResponse) throws Exception {
+        //jacson라이브러리의 클래스인 ObjectMapper, JSON <-> 문자열
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //알 수 없는 속성 무시
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //Root에 래핑 해제(JSON의 최상위 키를 자동으로 언래핑)
+        objectMapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+
+        ToiletListResponse response = objectMapper.readValue(jsonResponse, ToiletListResponse.class);
+
+        return response;
     }
 }
