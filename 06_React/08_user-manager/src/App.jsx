3e3933d398b6628cb5b461dea5862453b0d8ecfb
Hyeokjina <lsh6439ll@gmail.com>
2025-11-28 11:01:01 +0900

    feat(08_user-project) : add 08_user-project file - 08_user-project 회원등록 관리페이지 만들기

---
+++ 06_React/08_user-manager/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserList from './Pages/UserList';
+import UserDetail from './Pages/UserDetail';
+import UserRegistration from './Pages/UserRegistration';
+import NotFound from './Pages/NotFound';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUser = (user) => {
+    const newUser = { ...user, id: Date.now() };
+    setUsers([...users, newUser]);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter(user => user.id !== parseInt(id)));
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList users={users} />} />
+        <Route path="/user" element={<UserRegistration addUser={addUser} />} />
+        <Route path="/user/:id" element={<UserDetail users={users} deleteUser={deleteUser} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
