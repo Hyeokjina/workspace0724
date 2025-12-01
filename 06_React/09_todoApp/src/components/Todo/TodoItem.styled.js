fb464c1ac5aa38b47b9516603cd0744b2628b34c
Hyeokjina <lsh6439ll@gmail.com>
2025-12-01 14:51:53 +0900

    feat(todo) add todoitem.styled.js - todoitem.styled.js 목록 리스트 스타일 생성

--- 06_React/09_todoApp/src/components/Todo/TodoItem.styled.js
+++ 06_React/09_todoApp/src/components/Todo/TodoItem.styled.js
@@ -1,39 +1,48 @@
-import styled from "styled-components"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const TodoItemContainer = styled.li`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 16px;
+    padding: 6px 12px;
     border: 1px solid #e3e3e3;
-    border-radius: 4px;
-    margin-bottom: 8px;
-    background: white;
+
+`
+
+export const TodoContent = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    flex: 1;
 `
 
 export const TodoText = styled(Link)`
     text-decoration: ${props => props.completed ? 'line-through' : 'none'};
-    color: ${props => props.completed ? '#999' : '#037e21ff'};
+    color: ${props => props.completed ? '#999' : '#24854cff'};
     cursor: pointer;
-    transition: all 0.2s;
+    transition: color 0.2s;
 
     &:hover{
-        transform: scale(0.98);
+        scale: 0.98;
     }
 `
 
 export const Checkbox = styled.input`
+    width: 18px;
+    height: 18px;
+    cursor: pointer;
 `
 
 export const DeleteButton = styled.button`
-    color: #bb1111ff;
+    color: #a50303ff;
     background: none;
     border: none;
     cursor: pointer;
     outline: none;
     padding: 4px 8px;
-    
+
     &:hover{
-        transform: scale(0.98);  
+        scale: 0.98;
     }
 `
