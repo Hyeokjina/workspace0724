c98c81917d23a738ce76ad5600f94c7cc344c04f
Hyeokjina <lsh6439ll@gmail.com>
2025-12-01 14:51:16 +0900

    feat(todo) add todoitem.jsx - todoitem.jsx 목록 리스트 컨포넌트 생성

--- 06_React/09_todoApp/src/components/Todo/TodoItem.jsx
+++ 06_React/09_todoApp/src/components/Todo/TodoItem.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { DeleteButton, TodoContent, TodoItemContainer, TodoText, Checkbox } from './TodoItem.styled'
+import React, { useEffect } from 'react'
+import { Checkbox, DeleteButton, TodoContent, TodoItemContainer, TodoText } from './TodoItem.styled'
 
-const TodoItem = ({todo}) => {
-  return (
-    <TodoItemContainer>
-        <TodoContent>
-            <Checkbox 
-                type='checkbox'
-                checked={todo.completed}
-                onChange={() => {/* 체크박스 토글 로직 */}}
-            />
-            <TodoText to={`/todos/${todo.id}`} completed={todo.completed ? 'true' : undefined}>
-              {todo.text}
-            </TodoText>
-        </TodoContent>
-        <DeleteButton>삭제</DeleteButton>
-    </TodoItemContainer>
-  )
+const TodoItem = ({todo, onToggle, onDelete}) => {
+
+    useEffect(() => {
+        console.log(todo)
+    },[todo])
+
+    return (
+        <TodoItemContainer>
+            <TodoContent>
+                <Checkbox 
+                    type='checkbox'
+                    checked={todo.completed}
+                    onChange={() => onToggle(todo.id)}
+                />
+                <TodoText to={`/todos/${todo.id}`} completed={todo.completed ? 'true' : undefined}>
+                    {todo.text}
+                </TodoText>
+            </TodoContent>
+            <DeleteButton onClick={()=> onDelete(todo.id)}>삭제</DeleteButton>
+        </TodoItemContainer>
+    )
 }
 
 export default TodoItem
