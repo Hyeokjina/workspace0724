c1976273a5777a775e258be4b34a90e15c46ac29
Hyeokjina <lsh6439ll@gmail.com>
2025-12-01 17:48:31 +0900

    fix(context) add todoitem.jsx - todoContext.jsx 목록 컨포넌트 수정

--- 06_React/09_todoApp/src/components/Todo/TodoItem.jsx
+++ 06_React/09_todoApp/src/components/Todo/TodoItem.jsx
@@ -1,25 +1,23 @@
 import React, { useEffect } from 'react'
-import { Checkbox, DeleteButton, TodoContent, TodoItemContainer, TodoText } from './TodoItem.styled'
 
-const TodoItem = ({todo, onToggle, onDelete}) => {
+function TodoItem({ todo, onToggle, onDelete }) {
 
     useEffect(() => {
         console.log(todo)
-    },[todo])
+    }, [todo])
 
     return (
         <TodoItemContainer>
             <TodoContent>
-                <Checkbox 
+                <Checkbox
                     type='checkbox'
                     checked={todo.completed}
-                    onChange={() => onToggle(todo.id)}
-                />
+                    onChange={() => onToggle(todo.id)} />
                 <TodoText to={`/todos/${todo.id}`} completed={todo.completed ? 'true' : undefined}>
                     {todo.text}
                 </TodoText>
             </TodoContent>
-            <DeleteButton onClick={()=> onDelete(todo.id)}>삭제</DeleteButton>
+            <DeleteButton onClick={() => onDelete(todo.id)}>삭제</DeleteButton>
         </TodoItemContainer>
     )
 }
