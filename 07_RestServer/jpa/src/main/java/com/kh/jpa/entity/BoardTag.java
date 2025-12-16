f65ff5810c232a8fff8b6bcc534bc1d55fc963ba
Hyeokjina <lsh6439ll@gmail.com>
2025-12-16 14:05:08 +0900

    delete(boardTag) : delete file - boardTag 삭제

--- 07_RestServer/jpa/src/main/java/com/kh/jpa/entity/BoardTag.java
+++ 07_RestServer/jpa/src/main/java/com/kh/jpa/entity/BoardTag.java
@@ -3,22 +3,25 @@ package com.kh.jpa.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Builder
+@AllArgsConstructor
 @Entity
-@Table(name = "BOARD_TAG")
+@Table(name = "board")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-@IdClass(BoardTagId.class)
 public class BoardTag {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardTagId;
+
+    //==== 연관관계 ====
+    //게시글태그 : 게시글 (N : 1)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "BOARD_NO")
+    @JoinColumn(name = "board_no", nullable = false)
     private Board board;
 
-    @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TAG_ID")
+    @JoinColumn(name = "tag_id", nullable = false)
     private Tag tag;
 }
