94bde34baa41642803c8abd5224100c631ff932b
Hyeokjina <lsh6439ll@gmail.com>
2025-12-22 17:10:12 +0900

    .

--- 06_React/diary-rest-jpa/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
--- Emotion 초기 데이터 (중복 시 무시)
-MERGE INTO emotion (name, description) KEY(name) VALUES ('happy', '좋았어');
-MERGE INTO emotion (name, description) KEY(name) VALUES ('sad', '힘들어..');
-MERGE INTO emotion (name, description) KEY(name) VALUES ('normal', '그냥 그래');
-MERGE INTO emotion (name, description) KEY(name) VALUES ('fire', '최고!');
-
